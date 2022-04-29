c7a1aa8d34a182263c608aaca0d83a32288f8d3b
João Pedro Lima Pirajá <jplp100@hotmail.com>
2022-04-29 10:29:53 -0300

    Adição dos componentes Header e Footer

--- src/routes.js
+++ src/routes.js
@@ -4,16 +4,20 @@ import Cadastro from "./pages/Cadastro";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Produtos from "./pages/Produtos"
+import Header from "./components/Header"
+import Footer from "./components/Footer";
 
 function Routes() {
     return(
         <BrowserRouter>
+        <Header/>
             <Switch>
                 <Route path="/cadastro" component={Cadastro} />
                 <Route path="/home" component={Home} />
                 <Route path="/login" component={Login} />
                 <Route path="/produtos" component={Produtos} />
             </Switch>
+        <Footer/>
         </BrowserRouter>
     )
 }
